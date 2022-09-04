8880c4d1e1babec46bd41d1f1af9ee2b29d4bf7e
Dariia Gaydukova <daria1171@gmail.com>
2022-09-04 14:29:46 +0300

    UTF 8

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         while (true) {
 
-            System.out.println("�������� ��������:\n 1.��������\n 2.��������\n 3.�������\n 4.�����");
+            System.out.println("Выберите операцию:\n 1.добавить\n 2.показать покупки\n 3.удалить\n 4.найти");
             String input = scanner.nextLine();
             int operation = Integer.parseInt(input);
             int productCount = 0;
@@ -18,24 +18,24 @@ public class Main {
             switch (operation) {
 
                 case 1:
-                    System.out.println("����� ������� �� ������ ��������?");
+                    System.out.println("Какую покупку хотите добавить?");
                     String product = scanner.nextLine();
                     productList.add(product);
                     productCount++;
-                    System.out.println("����� � �������: " + productCount + " �������");
+                    System.out.println("Итого в списке покупок: " + productCount + " покупок");
                     break;
 
                 case 3:
-                    System.out.println("����� ������� �� ������ �������? (������ ����� ��� ��������)");
+                    System.out.println("Какую покупку хотите удалить? (напишите номер или сам товар)");
                     String deleteProduct = scanner.nextLine();
 
                     try {
                         int delete = Integer.parseInt(deleteProduct);
                         productList.remove(delete);
-                        System.out.println("������� " + deleteProduct + " �������, ������ �������:");
+                        System.out.println("Покупка " + deleteProduct + " удалена, список ваших покупок:");
                     } catch (NumberFormatException e) {
                         productList.remove(deleteProduct);
-                        System.out.println("������� " + deleteProduct + " �������, ������ �������:");
+                        System.out.println("Покупка " + deleteProduct + " удалена, список ваших покупок:");
                     }
 
 
@@ -45,12 +45,11 @@ public class Main {
                         System.out.println(i + 1 + "." + productList.get(i) + "\n");
 
                     }
-
                     break;
 
 
                 case 4:
-                    System.out.println("������� ����� ��� ������");
+                    System.out.println("Введите текст для поиска:");
                     String query = scanner.nextLine();
                     String queryLower = query.toLowerCase();
                     for (int i = 0; i < productList.size(); i++) {
